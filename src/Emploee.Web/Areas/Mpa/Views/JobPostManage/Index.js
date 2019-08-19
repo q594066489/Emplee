53b102a4ca6b80a58e5bede4cac8fbd0d8a45dde
Grimes <594066489@qq.com>
2019-08-19 17:18:43 +0800

    1

--- src/Emploee.Web/Areas/Mpa/Views/JobPostManage/Index.js
+++ src/Emploee.Web/Areas/Mpa/Views/JobPostManage/Index.js
@@ -338,237 +338,67 @@
     });
 
     getJobPosts();
+
+
+
+
+
+
+
+
+
+    function getJson() {
+
+        $.ajax({
+            type: 'Get',
+            url: '/Mpa/JobPostManage/GetPersonandIDcard',
+            async: false,//使用同步的方式,true为异步方式
+            dataType: "Json",
+            success: function (data) {
+
+                console.log(data);
+                var datas = trans_tree(data);
+                console.log(trans_tree(data));
+            }
+        })
+
+    }
+    getJson();
+
+
+    function trans_tree(jsonData){
+	   		//result存储json数据转为树形结构后的结果。
+	   		//temp为临时对象，将json数据按照id值排序.
+	   		//len是json长度，用于循环遍历结束的条件
+		    var result = [], temp = {}, len = jsonData.length
+		    for(var i = 0; i < len; i++){
+		    	// 以id作为索引存储元素，可以无需遍历直接快速定位元素
+		        temp[jsonData[i]['value']] = jsonData[i] 
+		    }
+		    for(var j = 0; j < len; j++){
+		        var currentElement = jsonData[j]
+		        // 临时变量里面的当前元素的父元素，即pid的值，与找对应id值
+                var tempCurrentElementParent = temp[currentElement['parentid']];
+		        // 如果存在父元素，即如果有pid属性
+		        if (tempCurrentElementParent!="0") { 
+		        	// 如果父元素没有chindren键
+		          if (!tempCurrentElementParent['children']) { 
+		          	// 设上父元素的children键
+		            tempCurrentElementParent['children'] = [] 
+		          }
+		          // 给父元素加上当前元素作为子元素
+		          tempCurrentElementParent['children'].push(currentElement) 
+		        } 
+		        // 不存在父元素，意味着当前元素是一级元素
+		        else { 
+		            result.push(currentElement);
+		        }
+		    }
+		    return result;
+		}
+
 });
  
 
 
-//(function () {
-//    $(function () {
-
-//        var _$jobPostsTable = $('#JobPostsTable');
-//        var _jobPostService = abp.services.app.jobPost;
-
-//        var _permissions = {
-//            create: abp.auth.hasPermission("Pages.JobPost.CreateJobPost"),
-//            edit: abp.auth.hasPermission("Pages.JobPost.EditJobPost"),
-//            'delete': abp.auth.hasPermission("Pages.JobPost.DeleteJobPost")
-
-//        };
-
-
-//        var _createOrEditModal = new app.ModalManager({
-//            viewUrl: abp.appPath + 'Mpa/JobPostManage/CreateOrEditJobPostModal',
-//            scriptUrl: abp.appPath + 'Areas/Mpa/Views/JobPostManage/_CreateOrEditJobPostModal.js',
-//            modalClass: 'CreateOrEditJobPostModal'
-//        });
-
-
-
-
-//        _$jobPostsTable.bootstrapTable({
-//            abpMethod: _jobPostService.getPagedJobPosts,
-//            toolbar: '#toolbar', //工具按钮用哪个容器
-//            queryParams: function (param) {
-//                var abpParam = {
-
-//                    FilterText: $('#txtFilterText').val(),
-//                    Sorting: param.sort,
-//                    skipCount: param.offset,
-//                    maxResultCount: param.limit
-//                };
-//                return abpParam;
-//            },
-//            columns:
-
-//                [
-//                    {
-
-//                        field: 'id',
-//                        title: 'ID',
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '3%',
-//                        visible: false
-//                    },
-//                    {
-
-//                        field: 'companyId',
-//                        title: '企业编号',
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '3%',
-//                        visible: false
-//                    },
-//                    {
-//                        field: 'jobName',
-//                        title: app.localize('JobName'),
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '8%',
-//                    },
-//                    {
-//                        field: 'salaryMin',
-//                        title: app.localize('SalaryMin'),
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '8%',
-//                    },
-//                    {
-//                        field: 'salaryMax',
-//                        title: app.localize('SalaryMax'),
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '8%',
-//                    },
-//                    {
-//                        field: 'education',
-//                        title: app.localize('Education'),
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '8%',
-//                    },
-//                    {
-//                        field: 'experience',
-//                        title: app.localize('Experience'),
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '8%',
-//                    },
-//                    {
-//                        field: 'jobAddress',
-//                        title: app.localize('JobAddress'),
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '8%',
-//                    },
-//                    {
-//                        field: 'skillRequire',
-//                        title: app.localize('SkillRequire'),
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '8%',
-//                    },
-
-//                    {
-//                        field: 'jobSelect',
-//                        title: app.localize('JobSelect'),
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '8%',
-//                    },
-//                    {
-//                        field: 'publishDate',
-//                        title: app.localize('PublishDate'),
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '8%',
-//                        formatter: function (value, row, index) {
-//                            return moment(value).format('L');
-//                        }
-//                    },
-
-//                    {
-//                        field: 'isDelete',
-//                        title: app.localize('isDelete'),
-//                        halign: 'center',
-//                        align: 'center',
-//                        width: '8%',
-
-//                    },
-
-//                    {
-//                        field: 'actions',
-//                        title: '操作',
-//                        align: 'center',
-//                        width: '10%',
-//                        formatter: function (value, row, index) {
-//                            var actions = '';
-//                            actions += ' <a class="edit" href="javascript:void(0)" title="' + app.localize('Edit') + '" style="margin-left: 10px;"><i class="fa fa-edit"></i></a>';
-//                            actions += ' <a class="remove" href="javascript:void(0)" title="' + app.localize('Delete') + '" style="margin-left: 10px;"><i class="fa fa-remove"></i></a>';
-//                            return actions;
-//                        },
-//                        events: {
-//                            'click .edit': function (e, value, row, index) {
-//                                _createOrEditModal.open({ size: '1800', id: row.id });
-//                            },
-//                            'click .remove': function (e, value, row, index) {
-//                                deleteCompany(row);
-//                            }
-//                        }
-//                    }
-//                ]
-
-//        });
-
-//        //打开添加窗口SPA
-//        $('#CreateNewJobPostButton').click(function () {
-//            //可选生成的对话框大小{size:'lg'}or{size:'sm'}
-//            //需要到_createContainer方法中添加,_args.size
-//            _createOrEditModal.open();
-//        });
-
-
-
-
-//        //刷新表格信息
-//        $("#ButtonReload").click(function () {
-//            getJobPosts();
-//        });
-
-
-
-
-//        //模糊查询功能
-//        function getJobPosts(reload) {
-//            if (reload) {
-//                //_$companysTable.jtable('reload');
-//            } else {
-//                //_$companysTable.jtable('load', {
-//                //    filtertext: $('#CompanysTableFilter').val()
-//                //});
-//                _$jobPostsTable.bootstrapTable('removeAll').bootstrapTable('refresh');
-//            }
-//        }
-//        //
-//        //删除当前jobPost实体
-//        function deleteJobPost(jobPost) {
-//            abp.message.confirm(
-//                app.localize('JobPostDeleteWarningMessage', jobPost.companyId),
-//                function (isConfirmed) {
-//                    if (isConfirmed) {
-//                        _jobPostService.deleteJobPost({
-//                            id: jobPost.id
-//                        }).done(function () {
-//                            getJobPosts(true);
-//                            abp.notify.success(app.localize('SuccessfullyDeleted'));
-//                        });
-//                    }
-//                }
-//            );
-//        }
-
-
-
-//        //导出为excel文档
-//        $('#ExportJobPostsToExcelButton').click(function () {
-//            _jobPostService
-//                .getJobPostsToExcel({})
-//                .done(function (result) {
-//                    app.downloadTempFile(result);
-//                });
-//        });
-//        //搜索
-//        $('#GetJobPostsButton').click(function (e) {
-//            e.preventDefault();
-//            getJobPosts();
-//        });
-
-//        //制作JobPost事件,用于请求变化后，刷新表格信息
-//        abp.event.on('app.createOrEditJobPostModalSaved', function () {
-//            getJobPosts(true);
-//        });
-        
-//        getJobPosts();
-        
-//})();
-//});
+ 
