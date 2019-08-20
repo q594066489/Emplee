91e5c87e1859e6b00062d74d596f973625ad675d
Grimes <594066489@qq.com>
2019-08-20 22:09:13 +0800

    行业筛选功能完成

--- src/Emploee.Web/Areas/Mpa/Views/JobPostManage/Index.js
+++ src/Emploee.Web/Areas/Mpa/Views/JobPostManage/Index.js
@@ -3,80 +3,67 @@
 }).use(['form', "jquery", "cascader", "form"], function () {
     var $ = layui.jquery;
     var cascader = layui.cascader;
+    var datas = "";
+    function getJson() {
 
-    var data = [
-        {
-            value: 'A',
-            label: 'a',
-            children: [
-                {
-                    value: 'AA1',
-                    label: 'aa1',
-                },
-                {
-                    value: 'BB1',
-                    label: 'bb1'
-                }
-            ]
-        },
-        {
-            value: 'B',
-            label: 'b',
-            children: [
-                {
-                    value: 'AA2',
-                    label: 'aa2',
-                    children: [
-                        {
-                            value: 'AAA3',
-                            label: 'aaa3',
-                            children: [
-                                {
-                                    value: 'AAA3',
-                                    label: 'aaa3',
-                                    children: [
-                                        {
-                                            value: 'AAA3',
-                                            label: 'aaa3',
-                                        },
-                                        {
-                                            value: 'BBB3',
-                                            label: 'bbb3'
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                        {
-                            value: 'BBB3',
-                            label: 'bbb3'
-                        }
-                    ]
-                },
-                {
-                    value: 'BB2',
-                    label: 'bb2',
-                    children: [
-                        {
-                            value: 'AAA4',
-                            label: 'aaa4',
-                        },
-                        {
-                            value: 'BBB4',
-                            label: 'bbb4'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            value: 'C',
-            label: 'c',
+        $.ajax({
+            type: 'Post',
+            url: '/Mpa/JobPostManage/GetPersonandIDcard',
+            async: false,//使用同步的方式,true为异步方式
+            dataType: "Json",
+            success: function (data) {
+
+                console.log(data);
+                datas = getJsonTree(data, 0);
+                console.log(datas);
+            }
+        })
+    }
+    ///数据递归生成
+    var getJsonTree = function (data, parentId) {
+        var itemArr = [];
+        for (var i = 0; i < data.length; i++) {
+            var node = data[i];
+            if (node.parentid == parentId) {
+                var newNode = {};
+                //newNode.id = node.id;
+                newNode.value = node.value;
+
+                newNode.label = node.label;
+                //newNode.url = node.url;
+                //newNode.icon = node.icon;
+                newNode.children = getJsonTree(data, node.value);
+                itemArr.push(newNode);
+            }
         }
-    ]
+        return itemArr;
+    };
+    getJson();
+    //var data = [
+    //    { "id": "4", "parentId": "1", "name": "大家电" },
+    //    { "id": "5", "parentId": "1", "name": "生活电器" },
+    //    { "id": "1", "parentId": "0", "name": "家用电器" },
+    //    { "id": "2", "parentId": "0", "name": "服饰" },
+    //    { "id": "3", "parentId": "0", "name": "化妆" },
+    //    { "id": "7", "parentId": "4", "name": "空调" },
+    //    { "id": "8", "parentId": "4", "name": "冰箱" },
+    //    { "id": "9", "parentId": "4", "name": "洗衣机" },
+    //    { "id": "10", "parentId": "4", "name": "热水器" },
+    //    { "id": "11", "parentId": "3", "name": "面部护理" },
+    //    { "id": "12", "parentId": "3", "name": "口腔护理" },
+    //    { "id": "13", "parentId": "2", "name": "男装" },
+    //    { "id": "14", "parentId": "2", "name": "女装" },
+    //    { "id": "15", "parentId": "7", "name": "海尔空调" },
+    //    { "id": "16", "parentId": "7", "name": "美的空调" },
+    //    { "id": "19", "parentId": "5", "name": "加湿器" },
+    //    { "id": "20", "parentId": "5", "name": "电熨斗" }
+    //];
+    //var s = getJsonTree(data, 0);
+    //console.log(s);
+     
     var cas = cascader({
         elem: "#a",
-        data: data,
+        data: datas,
         // url: "/aa",
         // type: "post",
         // triggerType: "change",
@@ -84,73 +71,14 @@
         // where: {
         //     a: "aaa"
         // },
-        value: ["B", "BB2", "BBB4"],
+        //value: ["B", "BB2", "BBB4"],
         // changeOnSelect: true,
         success: function (valData, labelData) {
             console.log(valData, labelData);
         }
     });
     
-    var reloadData = [
-        {
-            value: 'A',
-            label: 'a',
-            children: [
-                {
-                    value: 'AA1',
-                    label: 'aa1',
-                },
-                {
-                    value: 'BB1',
-                    label: 'bb1'
-                }
-            ]
-        }
-    ]
-    $(".layui-btn").on("click", function () {
-        cas.reload({
-            data: reloadData,
-            value: ['A', 'BB1']
-        })
-    });
-    ///数据递归生成
-    var getJsonTree = function (data, parentId) {
-        var itemArr = [];
-        for (var i = 0; i < data.length; i++) {
-            var node = data[i];
-            if (node.parentId == parentId) {
-                var newNode = {};
-                newNode.id = node.id;
-                newNode.name = node.name;
-                newNode.url = node.url;
-                newNode.icon = node.icon;
-                newNode.nodes = getJsonTree(data, node.id);
-                itemArr.push(newNode);
-            }
-        }
-        return itemArr;
-    };
-    var data = [
-        { "id": "4", "parentId": "1", "name": "大家电" },
-        { "id": "5", "parentId": "1", "name": "生活电器" },
-        { "id": "1", "parentId": "0", "name": "家用电器" },
-        { "id": "2", "parentId": "0", "name": "服饰" },
-        { "id": "3", "parentId": "0", "name": "化妆" },
-        { "id": "7", "parentId": "4", "name": "空调" },
-        { "id": "8", "parentId": "4", "name": "冰箱" },
-        { "id": "9", "parentId": "4", "name": "洗衣机" },
-        { "id": "10", "parentId": "4", "name": "热水器" },
-        { "id": "11", "parentId": "3", "name": "面部护理" },
-        { "id": "12", "parentId": "3", "name": "口腔护理" },
-        { "id": "13", "parentId": "2", "name": "男装" },
-        { "id": "14", "parentId": "2", "name": "女装" },
-        { "id": "15", "parentId": "7", "name": "海尔空调" },
-        { "id": "16", "parentId": "7", "name": "美的空调" },
-        { "id": "19", "parentId": "5", "name": "加湿器" },
-        { "id": "20", "parentId": "5", "name": "电熨斗" }
-    ];
-    var s = getJsonTree(data, 0);
-    console.log(s);
+      
 
 
     var _$jobPostsTable = $('#JobPostsTable');
@@ -386,55 +314,8 @@
 
 
 
-    function getJson() {
-
-        $.ajax({
-            type: 'Get',
-            url: '/Mpa/JobPostManage/GetPersonandIDcard',
-            async: false,//使用同步的方式,true为异步方式
-            dataType: "Json",
-            success: function (data) {
-
-                console.log(data);
-                var datas = trans_tree(data);
-                console.log(trans_tree(data));
-            }
-        })
-
-    }
-    getJson();
-
-
-    function trans_tree(jsonData){
-	   		//result存储json数据转为树形结构后的结果。
-	   		//temp为临时对象，将json数据按照id值排序.
-	   		//len是json长度，用于循环遍历结束的条件
-		    var result = [], temp = {}, len = jsonData.length
-		    for(var i = 0; i < len; i++){
-		    	// 以id作为索引存储元素，可以无需遍历直接快速定位元素
-		        temp[jsonData[i]['value']] = jsonData[i] 
-		    }
-		    for(var j = 0; j < len; j++){
-		        var currentElement = jsonData[j]
-		        // 临时变量里面的当前元素的父元素，即pid的值，与找对应id值
-                var tempCurrentElementParent = temp[currentElement['parentid']];
-		        // 如果存在父元素，即如果有pid属性
-		        if (tempCurrentElementParent!="0") { 
-		        	// 如果父元素没有chindren键
-		          if (!tempCurrentElementParent['children']) { 
-		          	// 设上父元素的children键
-		            tempCurrentElementParent['children'] = [] 
-		          }
-		          // 给父元素加上当前元素作为子元素
-		          tempCurrentElementParent['children'].push(currentElement) 
-		        } 
-		        // 不存在父元素，意味着当前元素是一级元素
-		        else { 
-		            result.push(currentElement);
-		        }
-		    }
-		    return result;
-		}
+    
+     
 
 });
  
