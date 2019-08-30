efde123695d65b288db247d481c4fac41435aec5
q594066489 <594066489@qq.com>
2019-08-30 17:25:06 +0800

    多选

--- src/Emploee.Web/Areas/Mpa/Views/ApprovalManage/Index.js
+++ src/Emploee.Web/Areas/Mpa/Views/ApprovalManage/Index.js
@@ -23,8 +23,9 @@
 
 
         _$jobPostsTable.bootstrapTable({
-            abpMethod: _approvalService.getPagedApprovals ,
-            toolbar: '#toolbar', //工具按钮用哪个容器
+            abpMethod: _approvalService.getPagedApprovals,
+            toolbar: '#toolbar', //工具按钮用哪个容器，
+            striped: true,                      //是否显示行间隔色
             queryParams: function (param) {
                 var abpParam = {
 
@@ -36,123 +37,117 @@
                 return abpParam;
             },
             columns:
+                [{
+                    checkbox: true,
+                    visible: true                  //是否显示复选框  
+                },
+                {
+                    field: 'id',
+                    title: 'ID',
+                    halign: 'center',
+                    align: 'center',
+                    width: '3%',
+                    visible: false
+                },
+                {
 
-                [
-                    {
-
-                        field: 'id',
-                        title: 'ID',
-                        halign: 'center',
-                        align: 'center',
-                        width: '3%',
-                        visible: false
-                    },
-                    {
-
-                        field: 'companyId',
-                        title: '企业编号',
-                        halign: 'center',
-                        align: 'center',
-                        width: '3%',
-                        visible: false
-                    },
-                    {
-                        field: 'registerDate',
-                        title: app.localize('RegisterDate'),
-                        halign: 'center',
-                        align: 'center',
-                        width: '8%',
-                        formatter: function (value, row, index) {
-                            return moment(value).format('L');
+                    field: 'companyId',
+                    title: '企业编号',
+                    halign: 'center',
+                    align: 'center',
+                    width: '3%',
+                    visible: false
+                },
+                {
+                    field: 'registerDate',
+                    title: app.localize('RegisterDate'),
+                    halign: 'center',
+                    align: 'center',
+                    width: '8%',
+                    formatter: function (value, row, index) {
+                        return moment(value).format('L');
+                    }
+                },
+                {
+                    field: 'isPay',
+                    title: '已付款',
+                    halign: 'center',
+                    align: 'center',
+                    width: '8%',
+                    formatter: function (value, row, index) {
+                        if (value.isPay) {
+                            return "<span class=\"label label-success\"> 是</span>";
                         }
+                        return "<span class=\"label label-danger\"> 否</span>";
+                    }
+                },
+                {
+                    field: 'payAmount',
+                    title: app.localize('Education'),
+                    halign: 'center',
+                    align: 'center',
+                    width: '8%',
+                },
+                {
+                    field: 'payTime',
+                    title: app.localize('PayTime'),
+                    halign: 'center',
+                    align: 'center',
+                    width: '8%',
+                    formatter: function (value, row, index) {
+                        return moment(value).format('L');
+                    }
+                },
+                {
+                    field: 'coopTime',
+                    title: app.localize('CoopTime'),
+                    halign: 'center',
+                    align: 'center',
+                    width: '8%',
+                    formatter: function (value, row, index) {
+                        return moment(value).format('L');
+                    }
+                },
+                {
+                    field: 'weight',
+                    title: app.localize('Weight'),
+                    halign: 'center',
+                    align: 'center',
+                    width: '8%',
+                    visible: false
+                },
+                {
+                    field: 'actions',
+                    title: '操作',
+                    align: 'center',
+                    width: '10%',
+                    formatter: function (value, row, index) {
+                        var actions = '';
+                        actions += ' <a class="edit" href="javascript:void(0)" title="' + app.localize('Edit') + '" style="margin-left: 10px;"><i class="fa fa-edit"></i></a>';
+                        return actions;
                     },
-                    {
-                        field: 'isPay',
-                        title: '已付款',
-                        halign: 'center',
-                        align: 'center',
-                        width: '8%',
-                        formatter: function (value, row, index) {
-
-                            if (value.isPay) {
-                                return "<span class=\"label label-success\"> 是</span>";
-                            }
-
-                            return "<span class=\"label label-danger\"> 否</span>";
-
-                        }
-                    },
-
-                    {
-                        field: 'payAmount',
-                        title: app.localize('Education'),
-                        halign: 'center',
-                        align: 'center',
-                        width: '8%',
-                    },
-                     
-                    {
-                        field: 'payTime',
-                        title: app.localize('PayTime'),
-                        halign: 'center',
-                        align: 'center',
-                        width: '8%',
-                        formatter: function (value, row, index) {
-                            return moment(value).format('L');
-                        }
-                    },
-                    {
-                        field: 'coopTime',
-                        title: app.localize('CoopTime'),
-                        halign: 'center',
-                        align: 'center',
-                        width: '8%',
-                        formatter: function (value, row, index) {
-                            return moment(value).format('L');
-                        }
-                    },
-                    {
-                        field: 'weight',
-                        title: app.localize('Weight'),
-                        halign: 'center',
-                        align: 'center',
-                        width: '8%',
-                        visible: false
-                    },
-                     
-
-                    {
-                        field: 'actions',
-                        title: '操作',
-                        align: 'center',
-                        width: '10%',
-                        formatter: function (value, row, index) {
-                            var actions = '';
-                            actions += ' <a class="edit" href="javascript:void(0)" title="' + app.localize('Edit') + '" style="margin-left: 10px;"><i class="fa fa-edit"></i></a>';                            
-                            return actions;
-                        },
-                        events: {
-                            'click .edit': function (e, value, row, index) {
-                                _createOrEditModal.open({ size: '1800', id: row.id });
-                            } 
+                    events: {
+                        'click .edit': function (e, value, row, index) {
+                            _createOrEditModal.open({ size: '1800', id: row.id });
                         }
                     }
+                }
                 ]
 
         });
-         
+
         //打开添加窗口SPA
         $('#CreateNewApprovalButton').click(function () {
             //可选生成的对话框大小{size:'lg'}or{size:'sm'}
             //需要到_createContainer方法中添加,_args.size
             _createOrEditModal.open();
         });
-         //模糊查询功能
+        //模糊查询功能
         function getApprovals(reload) {
             _$approvalsTable.bootstrapTable('removeAll').bootstrapTable('refresh');
-             
+
         }
-         
+
         //搜索
         $('#GetApprovalsButton').click(function (e) {
             e.preventDefault();
@@ -163,7 +158,7 @@
             getApprovals();
         });
         getApprovals();
-         
+
 
     });
 })();
