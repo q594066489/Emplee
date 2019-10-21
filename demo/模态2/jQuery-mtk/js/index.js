925d5091c1ff954f8b637d3e1614327ffb69df79
Grimes <594066489@qq.com>
2019-10-21 17:45:37 +0800

    1

--- demo/模态2/jQuery-mtk/js/index.js
+++ demo/模态2/jQuery-mtk/js/index.js
@@ -1,10 +1,62 @@
 $(function(){ 
+     var  input={
+          education:'',
+          salary:'',
+          experience:'',
+          scale:'',
+          finance:'',
+          SetValueByClass:function (_class,_value){
+               switch(_class){
+                   case 'education':
+                       if(_value!=null){
+                            this.education=_value;
+                       }
+                       break;
+                    case 'salary':
+                        if(_value!=null){
+                            this.salary=_value;
+                        }
+                        break;
+                    case 'experience':
+                       if(_value!=null){
+                            this.experience=_value;
+                       }
+                       break;   
+                    case 'scale':
+                        if(_value!=null){
+                            this.scale=_value;
+                        }
+                        break;  
+                    case 'finance':
+                            if(_value!=null){
+                                this.finance=_value;
+                            }
+                        break; 
+                    default:
+                        break;
+               }
+          }
+     }
+    //  function SetValueByClass(_class,_value){
+    //         switch
 
+    //  }
      $(".divbtn").click(function(){
 
-         $(this).siblings().removeClass('divclick');
-         $(this).addClass('divclick');
+        $(this).siblings().removeClass('divclick');
+        $(this).addClass('divclick');
+        console.log('class:---')
+        console.log($(this).attr('data-class'));
+        console.log('value:---')
+        console.log($(this).attr('data-value'));
+        input.SetValueByClass($(this).attr('data-class'),$(this).attr('data-value'));
+        
+        console.log(input);
+
+
      })
-    }); 
+
+
+}); 
     
      
